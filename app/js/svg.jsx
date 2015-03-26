e9dc96751e74cef6e3b81ca0545c7bdcbdddc4ec
Justin McCandless <justin.mccandless@teespring.com>
2015-03-26 22:44:30 +0800

    Do not change size of svg's rect with size of svg.  More intuitive for learning viewbox

--- app/js/svg.jsx
+++ app/js/svg.jsx
@@ -9,7 +9,7 @@ var ViewboxSVG = React.createClass({
         viewBox={this.props.viewboxMinX + ', ' + this.props.viewboxMinY + ', ' + this.props.viewboxWidth + ', ' + this.props.viewboxHeight}
         preserveAspectRatio={this.props.align + ' ' + this.props.meetOrSlice}
         version="1.1">
-        <rect xmlns="http://www.w3.org/2000/svg" x="0" y="0" width={this.props.width + 'px'} height={this.props.height + 'px'} fill="black"/>
+        <rect xmlns="http://www.w3.org/2000/svg" x="0" y="0" width="100" height="100" fill="black"/>
         <g xmlns="http://www.w3.org/2000/svg">
           <circle cx="50" cy="50" r="30" fill="yellow"/>
           <circle cx="40" cy="40" r="4" fill="black"/>
